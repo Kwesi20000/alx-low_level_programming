1509ccc7da1a57ce8774e0acd282efd08faff91d
Kwesi20000 <hemdrickskumi@yahoo.com>
2023-06-16 16:26:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
