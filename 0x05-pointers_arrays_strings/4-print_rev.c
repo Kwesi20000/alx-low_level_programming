70ddd77aa6b2a7cb8e3a8bdba93a6d7cbb1a93bf
Kwesi20000 <hemdrickskumi@yahoo.com>
2023-06-26 13:42:32 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+
+while (*s)
+{
+length++;
+s++;
+}
+
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+
+_putchar('\n');
+}
