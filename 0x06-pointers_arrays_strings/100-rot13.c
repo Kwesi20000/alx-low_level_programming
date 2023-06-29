5920a4207c20d6fa5742282e5b98b2c78070741a
Kwesi20000 <hemdrickskumi@yahoo.com>
2023-06-29 10:14:10 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,36 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - Encodes a string using ROT13 cipher.
- * @str: The string to encode.
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
- * Return: Pointer to the encoded string.
+ * Return: *s
  */
-char *rot13(char *str)
-{
-int i, j;
-char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-/* Iterate through each character in the string */
-for (i = 0; str[i] != '\0'; i++)
+char *rot13(char *s)
 {
-/* Check if the character is a letter */
-if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-{
-/* Iterate through each letter in the letters array */
-for (j = 0; j < 52; j++)
-{
-/* Check if the current character matches a letter */
-if (str[i] == letters[j])
-{
-/* Replace the letter with the corresponding ROT13 character */
-str[i] = rot13[j];
-break;
-}
-}
-}
-}
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-return (str);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
