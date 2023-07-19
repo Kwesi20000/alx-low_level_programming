b09b33f5a3b5775dad9509a8e87f9c2bcbc32b24
Kwesi20000 <hemdrickskumi@yahoo.com>
2023-07-19 11:04:20 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,44 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - Entry point
- * @argc: The number of command line arguments
- * @argv: An array containing the command line arguments
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, 98 on wrong number of arguments,
- *         99 on invalid operator, or 100 on division by zero
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int num1, num2, result;
-int (*op_func)(int, int);
+	int num1, num2;
+	char *op;
 
-if (argc != 4)
-{
-printf("Error\n");
-return (98);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op_func = get_op_func(argv[2]);
-
-if (op_func == NULL)
-{
-printf("Error\n");
-return (99);
-}
-
-if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-{
-printf("Error\n");
-return (100);
-}
-
-result = op_func(num1, num2);
-printf("%d\n", result);
-
-return (0);
-}
