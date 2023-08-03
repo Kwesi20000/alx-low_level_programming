4e575bd650994ff8d601f8fdf614ee30f68b103b
Kwesi20000 <hendrickskumi@yahoo.com>
2023-08-03 11:53:39 -0700

    main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Task 0: Binary to Unsigned Int */
+unsigned int binary_to_uint(const char *b);
+
+/* Task 1: Print Binary */
+void print_binary(unsigned long int n);
+
+/* Task 2: Get Bit */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* Task 3: Set Bit to 1 */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* Task 4: Clear Bit to 0 */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* Task 5: Flip Bits */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif /* MAIN_H */
+
